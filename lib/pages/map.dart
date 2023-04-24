66c7e699d30a0ded0f76077a560075996c305cdb
GawinGowin <101625248+GawinGowin@users.noreply.github.com>
2023-04-24 12:40:27 +0900

    add mappage

--- lib/pages/map.dart
+++ lib/pages/map.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class MapPage extends StatelessWidget {
+class MapPage extends StatefulWidget {
   const MapPage({super.key});
+
+  @override
+  State<MapPage> createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  late GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(45.521563, -122.677433);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,11 +26,13 @@ class MapPage extends StatelessWidget {
         centerTitle: true,
         elevation: 10,
       ),
-      body: Placeholder(),
-
-
-
-
-    );
+      body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 11.0,
+          ),
+        ),
+      );
   }
 }
