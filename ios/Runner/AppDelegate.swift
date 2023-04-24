89c2ab05cc2c11ffecadacedd33c9af7ecf42797
GawinGowin <101625248+GawinGowin@users.noreply.github.com>
2023-04-24 18:01:57 +0900

    add API-KEY placeholder

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -9,6 +9,8 @@ import GoogleMaps
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    // TODO: Add your Google Maps API key
+    GMSServices.provideAPIKey("YOUR-API-KEY")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
